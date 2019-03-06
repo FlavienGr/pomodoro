1835b714ef862771ca1eb4c10c60b2c193e65671
Flavor85 <flaviengr75@gmail.com>
2019-03-06 15:18:33 +0100

    Updates

---
+++ precache-manifest.973915a54640e1c06efa8f6fe49689ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c863a22e0a7e19d99e11",
+    "url": "/pomodoro/static/css/main.4cb18c5a.chunk.css"
+  },
+  {
+    "revision": "c863a22e0a7e19d99e11",
+    "url": "/pomodoro/static/js/main.c863a22e.chunk.js"
+  },
+  {
+    "revision": "d439e8744bee3815eb5d",
+    "url": "/pomodoro/static/js/1.d439e874.chunk.js"
+  },
+  {
+    "revision": "a702e67ff471c6ecfc33",
+    "url": "/pomodoro/static/js/runtime~main.a702e67f.js"
+  },
+  {
+    "revision": "83dacc2cea975f6a4406dc6d702f7564",
+    "url": "/pomodoro/index.html"
+  }
+];
